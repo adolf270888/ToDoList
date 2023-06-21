2bc33663ca85d0f2c31e04fcc8f9dd35ff53959e
adolf270888 <77062562+adolf270888@users.noreply.github.com>
2023-06-21 08:54:31 +0000

    Добавление удаления

--- ToDoList/ToDoList/Controllers/HomeController.cs
+++ ToDoList/ToDoList/Controllers/HomeController.cs
@@ -118,6 +118,19 @@ namespace ToDoList.Controllers
             return View("AddTask", taskList);
         }
 
+         [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                TaskList taskList = new TaskList {Id=id.Value };
+                toDoListDbContext.Entry(taskList).State = EntityState.Deleted;
+                await toDoListDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+
 
         public IActionResult Privacy()
         {
